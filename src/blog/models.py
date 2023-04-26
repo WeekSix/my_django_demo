e3b36b0a845de9de5d9ea29e37c4a661bbcdaf26
zoey <zoeyzhou6@gmail.com>
2023-04-25 21:02:23 -0400

    Lecture 27

--- src/blog/models.py
+++ src/blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class BlogPost(models.Model):
     title = models.TextField()
     content = models.TextField(null=True, blank=True)
+    # picture = models.ImageField()
 
 
 class Comment(models.Model):
--- src/blog/views.py
+++ src/blog/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import BlogPost
+from .models import Comment
 
-article = BlogPost.objects.get(id=1)
+# article = BlogPost.objects.get(id=1)
 
 
 def blog_post_detail_page(request):
     article = BlogPost.objects.get(id=1)  # query -> database -> data -> django render it
+    comments = []
+    for _ in range(1,3):
+        comments.append(Comment.objects.get(id=_))
     template_name = 'blog_post_detail.html'
-    context = {"object": article}
+    context = {"object": article, "comments_list": comments}
     return render(request, template_name, context)
